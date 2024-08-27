1c86508cbf7770d50322158eec8443f335d43590
AYUSH SRIVASTAVA <ayushsrivastava1011@gmail.com>
2024-08-27 10:33:56 +0530

    new section added

--- client/src/app/component/home/home.component.ts
+++ client/src/app/component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  @ViewChild("aboutus") aboutus!: ElementRef;
+  @ViewChild("contactus") contactus!: ElementRef;
+
+  constructor() { }
+
+  focus(section: any) {
+    section == 'aboutus' ? this.aboutus.nativeElement.scrollIntoView() : this.contactus.nativeElement.scrollIntoView();
+
+  }
 
 }
