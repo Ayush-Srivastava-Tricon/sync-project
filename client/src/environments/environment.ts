bb67c9d79813e7be3f184bd4b62d0e43cef801a7
AYUSH SRIVASTAVA <ayushsrivastava1011@gmail.com>
2024-11-04 09:46:44 +0530

    Production url updated

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    baseurl:`${window.location.protocol}//45.133.178.176:5000/uploads`,
-    url:`${window.location.protocol}//45.133.178.176:5000/api`
+    baseurl:`${window.location.protocol}//roomdeals.travel/uploads`,
+    url:`${window.location.protocol}//roomdeals.travel/api`
 };
